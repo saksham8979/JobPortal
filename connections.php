2c8bbbfdff8944dd5b19a4d4ef84a399b5adc652
saksham8979 <sakshamjohri@gmail.com>
2020-07-19 13:03:49 +0530

    Update connections.php

--- connections.php
+++ connections.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
-	$conn = mysqli_connect("localhost","root","usbw") or die(mysqli_connect_error());
-	$DB = mysqli_select_db($conn,'squareBoat') or die(mysqli_error($conn));
+	$conn = mysqli_connect("us-cdbr-east-02.cleardb.com","b82a0276ea0d73","51f64732") or die(mysqli_connect_error());
+	$DB = mysqli_select_db($conn,'heroku_db4da753d301a40') or die(mysqli_error($conn));
 	date_default_timezone_set('Asia/Kolkata');
 	
-	$APP_URL = 'http://localhost:8080/SquareBoat/';
+	$APP_URL = 'https://job-profile.herokuapp.com/';
 ?>
